5f0e412a04f2a070126fc4e2b7958940b6fe59f3
Louis <innocentlouis1@gmail.com>
2018-05-29 12:07:06 +0000

    Premier commit

---
+++ sites/all/themes/formation_theme/Cpage.tpl.php
@@ -0,0 +1,11 @@
+<div class="container">
+<h2>Prêt à débuter</h2>
+
+<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+<div><a class="btn btn-default btn-lg my-btn" href="#">Debuter</a></div>
+
+<div class="margin-vertical-50"><img alt="" height="32" src="/sites/default/files/twiter.jpg" width="32" /><img alt="" height="32" src="/sites/default/files/link.jpg" width="32" /><img alt="" height="32" src="/sites/default/files/facebook.jpg" width="32" /></div>
+
+<p>Copyright (c) 2017 Copyright Holder All Rights Reserved.</p>
+</div>
